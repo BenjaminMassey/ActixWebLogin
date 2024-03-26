0ce3a17fab2aaf8127ecbf2d01c8ad8a6170e32e
Benjamin Massey <benjamin.w.massey@gmail.com>
2024-03-26 09:57:44 -0700

    Using rusqlite for sqlite storage of user info, instead of text file

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use actix_identity::Identity;
 use actix_web::web;
 use serde::Deserialize;
 use actix_web::web::Redirect;
+use rusqlite::{Connection, Result};
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use pbkdf2::{
@@ -30,37 +31,25 @@ async fn index(user: Option<Identity>) -> impl Responder {
 }
 
 #[derive(Deserialize)]
-struct Info {
+struct FormInfo {
     name: String,
     password: String,
 }
 
 #[post("/do_login")]
-async fn do_login(request: HttpRequest, web::Form(form): web::Form<Info>) -> impl Responder {
-    let mut verified = false;
-    let file = File::open("accounts.txt").unwrap();
-    let reader = BufReader::new(file);
-    for raw_line in reader.lines() {
-        let line = raw_line.unwrap();
-        let pieces: Vec<&str> = line.split(" : ").collect();
-        let this_info = (pieces[0].to_owned(), pieces[1].to_owned());
-        if this_info.0 == form.name.clone() &&
-            Pbkdf2.verify_password(
-                form.password.clone().as_bytes(),
-                &PasswordHash::new(&this_info.1).unwrap()
-            ).is_ok() {
-            verified = true;
-            break;
-        }
+async fn do_login(request: HttpRequest, web::Form(form): web::Form<FormInfo>) -> impl Responder {
+    let result = get_user_info_sqlite(form.name.clone());
+    if result.is_none() {
+        return Redirect::to("/login/User%20Not%20Found").see_other();
     }
-
-    if verified {
-        // attach a verified user identity to the active session
-        Identity::login(&request.extensions(), form.name.clone().into()).unwrap();
-        return Redirect::to("/").see_other();
+    if Pbkdf2.verify_password(
+            form.password.clone().as_bytes(),
+            &PasswordHash::new(&result.unwrap().password).unwrap()
+        ).is_err() {
+        return Redirect::to("/login/Password%20Is%20Incorrect").see_other();
     }
-
-    Redirect::to("/login/User%20Not%20Found").see_other() // TODO: message
+    Identity::login(&request.extensions(), form.name.clone().into()).unwrap();
+    Redirect::to("/").see_other()
 }
 #[get("/login")]
 async fn login() -> impl Responder {
@@ -76,20 +65,14 @@ fn login_html(message: &str) -> String {
 }
 
 #[post("/do_create_account")]
-async fn do_create_account(request: HttpRequest, web::Form(form): web::Form<Info>) -> impl Responder {
-    let file = File::open("accounts.txt").unwrap();
-    let reader = BufReader::new(file);
-    for raw_line in reader.lines() {
-        let line = raw_line.unwrap();
-        let pieces: Vec<&str> = line.split(" : ").collect();
-        if pieces[0].to_owned() == form.name.clone() {
-            return Redirect::to("/create_account/Username%20already%20exists.").see_other();
-        }
+async fn do_create_account(request: HttpRequest, web::Form(form): web::Form<FormInfo>) -> impl Responder {
+    let result = get_user_info_sqlite(form.name.clone());
+    if result.is_some() {
+        return Redirect::to("/create_account/Username%20already%20exists.").see_other();
     }
     let salt = SaltString::generate(&mut OsRng);
     let password_hash = Pbkdf2.hash_password(form.password.as_bytes(), &salt).unwrap().to_string();
-    let mut file = File::options().write(true).append(true).open("accounts.txt").unwrap();
-    file.write(format!("{} : {}\n", form.name.clone(), password_hash).as_bytes()).unwrap();
+    let _ = insert_user_sqlite(form.name.clone(), password_hash);
     Identity::login(&request.extensions(), form.name.clone().into()).unwrap();
     Redirect::to("/").see_other()
 }
@@ -114,12 +97,49 @@ async fn logout(user: Option<Identity>) -> impl Responder {
     Redirect::to("/").see_other() // TODO: messaging
 }
 
+fn insert_user_sqlite(username: String, password: String) -> Result<()> {
+    let mut conn = Connection::open("accounts.db")?;
+    let tx = conn.transaction()?;
+    tx.execute(
+        "INSERT INTO users (username, password) VALUES (?1, ?2)",
+        &[&username, &password],
+    )?;
+    tx.commit()?;
+    Ok(())
+}
+
+struct UserInfo {
+    username: String,
+    password: String,
+}
+
+fn get_user_info_sqlite(username: String) -> Option<UserInfo> {
+    let conn = Connection::open("accounts.db").unwrap();
+    let mut stmt = conn.prepare("SELECT * FROM users WHERE username = ?1").unwrap();
+    let mut rows = stmt.query([&username]).unwrap();
+    if let Some(row) = rows.next().unwrap() {
+        //let username: String = row.get(0).unwrap();
+        let password: String = row.get(1).unwrap();
+        Some(UserInfo {username, password})
+    } else {
+        None
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // When using `Key::generate()` it is important to initialize outside of the
     // `HttpServer::new` closure. When deployed the secret key should be read from a
     // configuration file or environment variables.
     let secret_key = Key::generate();
+
+    let conn = Connection::open("accounts.db").unwrap();
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+             username TEXT PRIMARY KEY,
+             password TEXT)",
+        [],
+    ).unwrap();
 
     HttpServer::new(move || {
         App::new()
